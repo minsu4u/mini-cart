346b489d236b81de626dafa7d58a2a82ef8eee7a
minsu gawk <myminsu@naver.com>
2022-10-09 19:26:51 +0900

    10-09

---
+++ src/component/CartList.js
@@ -0,0 +1,107 @@
+const MAX_COUNT = 10;
+const MIN_COUNT = 1;
+
+class CartList {
+  constructor($target, initialData) {
+    this.$target = $target;
+    this.$container = document.createElement('ul');
+    this.$container.className = 'divide-y divide-gray-200';
+    this.$totalCount = document.getElementById('total-count');
+    this.state = initialData;
+    this.$target.append(this.$container);
+    this.render();
+  }
+  setState(newState) {
+    this.state = newState;
+    this.render();
+  }
+
+  addCartItem(productData) {
+    let newState;
+    const clickedProduct = productData.id;
+    const chekedIdx = this.state.findIndex(
+      (item) => item.id === clickedProduct
+    );
+    console.log(chekedIdx);
+    if (chekedIdx === -1) {
+      newState = [...this.state, { ...productData, count: 1 }];
+    } else {
+      newState = [...this.state];
+      newState[chekedIdx].count += 1;
+    }
+    this.setState(newState);
+  }
+  increaseCartItem(id) {
+    const newState = [...this.state];
+    const chekedIdx = this.state.findIndex((item) => item.id === id);
+    if (newState[chekedIdx].count < MAX_COUNT) {
+      newState[chekedIdx].count += 1;
+    } else {
+      alert('장바구니에 담을 수 있는 최대 수량은 1개입니다.');
+    }
+    this.setState(newState);
+  }
+  decreaseCartItem(id) {
+    const newState = [...this.state];
+    const chekedIdx = this.state.findIndex((item) => item.id === id);
+    if (newState[chekedIdx].count > MIN_COUNT) {
+      newState[chekedIdx].count -= 1;
+    } else {
+      alert('장바구니에 담을 수 있는 최소 수량은 1개입니다.');
+    }
+    this.setState(newState);
+  }
+  removeCartItem(id) {
+    const newState = this.state.filter((item) => item.id !== id);
+    this.setState(newState);
+  }
+  render() {
+    this.$totalCount.innerHTML =
+      this.state
+        .reduce((acc, cur) => cur.price * cur.count + acc, 0)
+        .toLocaleString() + '원';
+    this.$container.innerHTML = this.state
+      .map((item) => {
+        return `
+          <li class="flex py-6" id=${item.id}>
+                    <div
+                      class="h-24 w-24 overflow-hidden rounded-md border border-gray-200"
+                    >
+                      <img
+                        src=${item.imgSrc}
+                        class="h-full w-full object-cover object-center"
+                      />
+                    </div>
+                    <div class="ml-4 flex flex-1 flex-col">
+                      <div>
+                        <div
+                          class="flex justify-between text-base font-medium text-gray-900"
+                        >
+                          <h3>${item.name}</h3>
+                          <p class="ml-4">${(
+                            item.price * item.count
+                          ).toLocaleString()}원</p>
+                        </div>
+                      </div>
+                      <div class="flex flex-1 items-end justify-between">
+                        <div class="flex text-gray-500">
+                          <button class="decrease-btn">-</button>
+                          <div class="mx-2 font-bold">${item.count}개</div>
+                          <button class="increase-btn">+</button>
+                        </div>
+                        <button
+                          type="button"
+                          class="font-medium text-sky-400 hover:text-sky-500"
+                        >
+                          <p class="remove-btn">삭제하기</p>
+                        </button>
+                      </div>
+                    </div>
+                  </li>
+          `;
+      })
+      .join('');
+  }
+}
+
+export default CartList;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 // 이 곳에 정답 코드를 작성해주세요.
 import getProductData from './api/getProductData.js';
 import ProductList from './component/ProductList.js';
+import CartList from './component/CartList.js';
 
 const $productCardGrid = document.getElementById('product-card-grid');
 const $openCartBtn = document.getElementById('open-cart-btn');
 const $closeCartBtn = document.getElementById('close-cart-btn');
 const $shoppingCart = document.getElementById('shopping-cart');
 const $backdrop = document.getElementById('backdrop');
+const $cartList = document.getElementById('cart-list');
+
+let productData = [];
 
 const productList = new ProductList($productCardGrid, []);
+const cartList = new CartList($cartList, []);
 
 const toggleCart = () => {
   $shoppingCart.classList.toggle('translate-x-full');
@@ -18,8 +23,34 @@ const toggleCart = () => {
 
 const fetchProductData = async () => {
   const result = await getProductData();
-  console.log(result);
   productList.setState(result);
+  productData = result;
+};
+
+const addCartItem = (e) => {
+  const clickedProduct = productData.find(
+    (product) => product.id == e.target.dataset.productid
+  );
+  if (!clickedProduct) return;
+  cartList.addCartItem(clickedProduct);
+  toggleCart();
+};
+
+const modifyCartItem = (e) => {
+  const currentProductId = parseInt(e.target.closest('li').id);
+  switch (e.target.className) {
+    case 'increase-btn':
+      cartList.increaseCartItem(currentProductId);
+      break;
+    case 'decrease-btn':
+      cartList.decreaseCartItem(currentProductId);
+      break;
+    case 'remove-btn':
+      cartList.removeCartItem(currentProductId);
+      break;
+    default:
+      return;
+  }
 };
 
 fetchProductData();
@@ -27,4 +58,5 @@ fetchProductData();
 $openCartBtn.addEventListener('click', toggleCart);
 $closeCartBtn.addEventListener('click', toggleCart);
 $backdrop.addEventListener('click', toggleCart);
-$productCardGrid.addEventListener('click', toggleCart);
+$productCardGrid.addEventListener('click', addCartItem);
+$cartList.addEventListener('click', modifyCartItem);
